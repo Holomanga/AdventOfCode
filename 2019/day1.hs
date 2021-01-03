98efa9e509376b5f6b2cc8feb94216cac570d5de
holomanga <holomanga@gmail.com>
2021-01-03 20:06:51 +0000

    Day 1 in Haskell (I should really brush up on haskell)

---
+++ 2019/day1.hs
@@ -0,0 +1,27 @@
+import System.IO
+
+fuelForModule :: Int -> Int
+fuelForModule mass = div mass 3 - 2
+
+recursiveFuelForModule :: Int -> Int
+recursiveFuelForModule mass = 
+    if fuelForModule mass < 0 then
+        0
+    else
+        fuelForModule mass + recursiveFuelForModule (fuelForModule mass)
+
+part1 = do
+    handle <- openFile "2019/day1.txt" ReadMode
+    contents <- hGetContents handle
+    print (sum (map (fuelForModule . read) (lines contents) :: [Int]))
+    hClose handle
+
+part2 = do
+    handle <- openFile "2019/day1.txt" ReadMode
+    contents <- hGetContents handle
+    print (sum (map (recursiveFuelForModule . read) (lines contents) :: [Int]))
+    hClose handle
+
+main = do
+    part1
+    part2

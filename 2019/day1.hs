49bd6c510ca6a2075ea722ea977d49c8b91823bd
holomanga <holomanga@gmail.com>
2021-01-03 20:54:30 +0000

    Cleanup and data type headers

--- 2019/day1.hs
+++ 2019/day1.hs
@@ -1,4 +1,4 @@
-import System.IO
+import System.IO ( hClose, openFile, hGetContents, IOMode(ReadMode) )
 
 fuelForModule :: Int -> Int
 fuelForModule mass = div mass 3 - 2
@@ -10,18 +10,21 @@ recursiveFuelForModule mass =
     else
         fuelForModule mass + recursiveFuelForModule (fuelForModule mass)
 
+part1 :: IO ()
 part1 = do
     handle <- openFile "2019/day1.txt" ReadMode
     contents <- hGetContents handle
     print (sum (map (fuelForModule . read) (lines contents) :: [Int]))
     hClose handle
 
+part2 :: IO ()
 part2 = do
     handle <- openFile "2019/day1.txt" ReadMode
     contents <- hGetContents handle
     print (sum (map (recursiveFuelForModule . read) (lines contents) :: [Int]))
     hClose handle
 
+main :: IO ()
 main = do
     part1
     part2
